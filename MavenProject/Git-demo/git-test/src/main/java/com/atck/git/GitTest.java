df5e054fc7f34445e7e60f0cc970f001a7128577
kkkkk <kkkkk@atck.com>
2021-11-05 16:47:31 +0800

    activemq-demo-commit

---
+++ MavenProject/Git-demo/git-test/src/main/java/com/atck/git/GitTest.java
@@ -0,0 +1,22 @@
+package com.atck.git;
+
+/**
+ *
+ */
+public class GitTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello git");
+        System.out.println("hello git 2");
+        System.out.println("hello git 3");
+        System.out.println("hello git 4");
+        System.out.println("master test!");
+        System.out.println("hot-fix test");
+        System.out.println("push test!");
+        System.out.println("push test2!");
+        System.out.println("pull test!");
+        System.out.println("gitee test!");
+        System.out.println("gitee test2!");
+    }
+}
