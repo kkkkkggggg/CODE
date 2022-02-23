6d66583a3650434ec27122012a1dcb2d57c04e99
kkkkk <kkkkk@atck.com>
2022-02-23 16:27:07 +0800

    spring boot commit

---
+++ interview-test/src/main/java/com/atck/interview/InterviewTestApplication.java
@@ -0,0 +1,15 @@
+package com.atck.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTestApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(InterviewTestApplication.class, args);
+    }
+
+}
---
+++ interview-test/src/test/java/com/atck/interview/InterviewTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.atck.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTestApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
